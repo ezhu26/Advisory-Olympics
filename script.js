7acf54585182021bd8cbb749adc6255c43086edf
casecocke <casecocke@icloud.com>
2024-11-01 08:59:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// TODO: import libraries for Cloud Firestore Database
+// https://firebase.google.com/docs/firestore
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA48UmI50QVSpCJF6voYVudbChpVkFkU6g",
+    authDomain: "advisory-olympics.firebaseapp.com",
+    projectId: "advisory-olympics",
+    storageBucket: "advisory-olympics.appspot.com",
+    messagingSenderId: "975067253222",
+    appId: "1:975067253222:web:92eff5d86154b91641ee27"
+  };
