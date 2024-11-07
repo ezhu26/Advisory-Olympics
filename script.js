92a015492741781645dadd2d5da5886c5ea439bd
Jack  Adair <112512128+JackAdair-25@users.noreply.github.com>
2024-11-07 08:15:03 -0500

    Add files via upload
    Jack Round 1 Code

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebas
 // TODO: import libraries for Cloud Firestore Database
 // https://firebase.google.com/docs/firestore
 import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA48UmI50QVSpCJF6voYVudbChpVkFkU6g",
@@ -11,3 +12,42 @@ const firebaseConfig = {
     messagingSenderId: "975067253222",
     appId: "1:975067253222:web:92eff5d86154b91641ee27"
   };
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+
+export const login = async function(){
+signInWithPopup(auth, provider)
+  .then((result) => {
+    console.log("logging in")
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+}
+
+
+// var user;
+// if (user.login.username.includes("@stab.org")){
+//     displayEditPage = true;
+// }
+// if (displayEditPage = true){
+
+// }
